599146565d4f3dff0690fd4249a73203d9209a4c
abhishekjadhav <abhishekjadhav0015@gmail.com>
2026-09-21 16:03:42 +0530

    logger file problem solve

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pytorch-cnn-project",
+    version="0.0.1",
+    author="Abhishek Jadhav",  
+    author_email="abhishekjadhav0015@gmail.com",
+    packages=find_packages(),
+    description="End-to-end PyTorch CNN for Pneumonia Classification",
+)
