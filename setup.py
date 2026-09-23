cacbd38191b3f2bbc186f86a52a21d297159861a
abhishekjadhav <abhishekjadhav0015@gmail.com>
2026-09-23 16:03:56 +0530

    remove uv.lock  to disable uv builder

--- setup.py
+++ setup.py
@@ -1,30 +1,14 @@
-from typing import List
 from setuptools import find_packages, setup
-
-def get_requirements(file_path: str) -> List[str]: 
-    '''
-    This function will return list of requirements
-    '''
-    HYPEN_E_DOT = "-e ."
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        # Clean up newlines and spaces
-        requirements = [req.strip() for req in requirements]
-        
-        # Remove the -e . flag
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-            
-        # THE FIX: Ignore any pip flags like --extra-index-url
-        requirements = [req for req in requirements if not req.startswith("-")]
-        
-    return requirements
 
 setup(
     name="pneumonia-classifier",
     version="0.0.1",
-    author="Your Name",
+    author="Abhishek Jadav",
+    author_email="abhishekjadhav0015@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+    install_requires=[
+        "streamlit",
+        "pillow",
+        "tqdm",
+    ],
 )
